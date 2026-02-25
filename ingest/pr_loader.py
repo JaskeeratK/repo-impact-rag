aa1a61feaf4d050afe418dc943b947667a8e4166
Jaskeerat <169700703+JaskeeratK@users.noreply.github.com>
2026-02-25 20:39:33 +0530

    updated pr_loader

--- ingest/pr_loader.py
+++ ingest/pr_loader.py
@@ -1,1 +1,17 @@
+from github import Github
+import os
 
+
+class PRLoader:
+    def __init__(self, repo_name):
+        self.client = Github(os.getenv("GITHUB_TOKEN"))
+        self.repo = self.client.get_repo(repo_name)
+
+    def get_prs(self, limit=20):
+        prs = []
+        for pr in self.repo.get_pulls(state="all")[:limit]:
+            prs.append({
+                "title": pr.title,
+                "body": pr.body
+            })
+        return prs

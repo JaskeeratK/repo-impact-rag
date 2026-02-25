c16ec161e2747ada6659545873b2d0bd214a11bf
Jaskeerat <169700703+JaskeeratK@users.noreply.github.com>
2026-02-25 20:37:49 +0530

    updated repo_loader

--- ingest/repo_loader.py
+++ ingest/repo_loader.py
@@ -1,1 +1,15 @@
+import os
+import shutil
+from git import Repo
 
+
+class RepoLoader:
+    def __init__(self, repo_url, local_path="temp_repo"):
+        self.repo_url = repo_url
+        self.local_path = local_path
+
+    def clone(self):
+        if os.path.exists(self.local_path):
+            shutil.rmtree(self.local_path)
+        Repo.clone_from(self.repo_url, self.local_path)
+        return self.local_path

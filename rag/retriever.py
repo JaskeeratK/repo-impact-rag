485a5f205416705f7bc9956ab882e2bce3428e4b
Jaskeerat <169700703+JaskeeratK@users.noreply.github.com>
2026-02-25 20:42:35 +0530

    Update retriever.py

--- rag/retriever.py
+++ rag/retriever.py
@@ -1,1 +1,14 @@
+from embeddings.embedder import Embedder
+from vectorstore.store import VectorStore
 
+
+class Retriever:
+    def __init__(self):
+        self.embedder = Embedder()
+        self.store = VectorStore()
+
+    def retrieve(self, query):
+        query_embedding = self.embedder.embed(query)[0]
+        results = self.store.query(query_embedding)
+
+        return results

b9accb8cef6e3aba99a335e5f51a2411505619e3
Jaskeerat <169700703+JaskeeratK@users.noreply.github.com>
2026-02-25 20:41:29 +0530

    Replace FAISS with ChromaDB for vector storage
    Refactor vector store implementation to use ChromaDB instead of FAISS.

--- vectorstore/store.py
+++ vectorstore/store.py
@@ -1,4 +1,21 @@
-from langchain.vectorstores import FAISS
+import chromadb
 
-def build_store(docs, embedder):
-    return FAISS.from_documents(docs, embedder)
+
+class VectorStore:
+    def __init__(self, collection_name="repo_index"):
+        self.client = chromadb.Client()
+        self.collection = self.client.get_or_create_collection(collection_name)
+
+    def add(self, ids, documents, embeddings, metadatas):
+        self.collection.add(
+            ids=ids,
+            documents=documents,
+            embeddings=embeddings,
+            metadatas=metadatas
+        )
+
+    def query(self, query_embedding, n_results=5):
+        return self.collection.query(
+            query_embeddings=[query_embedding],
+            n_results=n_results
+        )

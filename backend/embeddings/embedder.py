ae86ef5a5cfa95e76ab42c5454952ba74ec56b1c
Jaskeerat <169700703+JaskeeratK@users.noreply.github.com>
2026-08-06 17:58:12 +0530

    Refactor Embedder class to use fastembed

--- backend/embeddings/embedder.py
+++ backend/embeddings/embedder.py
@@ -1,19 +1,3 @@
-# from sentence_transformers import SentenceTransformer
-
-
-# # class Embedder:
-# #     def __init__(self, model_name="sentence-transformers/all-MiniLM-L6-v2"):
-# #         self.model = SentenceTransformer(model_name)
-# from fastapi import Request
-
-# class Embedder:
-#     def __init__(self):
-#         self.model = SentenceTransformer("paraphrase-MiniLM-L3-v2")
-
-#     def embed(self, texts):
-#         if isinstance(texts, str):
-#             texts = [texts]
-#         return self.model.encode(texts).tolist()
 from fastembed import TextEmbedding
 
 
@@ -27,9 +11,6 @@ class Embedder:
 
     def embed(self, texts):
         if isinstance(texts, str):
-            texts = [texts]
-        embeddings = list(self.model.embed(texts))
-        return [e.tolist() for e in embeddings]
             texts = [texts]
         embeddings = list(self.model.embed(texts))
         return [e.tolist() for e in embeddings]
